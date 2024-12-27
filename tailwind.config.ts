2d76c3a15f721ae75ca42a7d16b952afcee30eab
Thea Deo <chanvuthea.eang@bi-kay.com>
2024-12-27 17:42:58 +0700

    fix config tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,5 +17,11 @@ export default {
     },
   },
   plugins: [],
-  purge: ["./src/**/*.html", "./src/**/*.js"],
+  purge: [
+    "./src/**/*.html",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
 } satisfies Config;
