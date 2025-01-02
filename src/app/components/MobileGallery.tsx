ae5c57166533bdfd19faeb28958f1142b7a581c4
Thea Deo <chanvuthea.eang@bi-kay.com>
2025-01-02 11:33:29 +0700

    fix img size

--- src/app/components/MobileGallery.tsx
+++ src/app/components/MobileGallery.tsx
@@ -1,8 +1,8 @@
 const MobileGallery = ({ imgList }: { imgList: string[] }) => {
   return (
-    <div className="flex flex-col w-full justify-between md:pl-16 md:items-center lg:flex-row lg:pl-4">
+    <div className="flex flex-col w-full justify-between md:pl-16 items-center lg:flex-row lg:pl-4">
       {imgList.map((img, index) => (
-        <div key={index} className="w-full h-full md:w-1/2 lg:w-1/4 pb-6 ">
+        <div key={index} className=" h-full w-1/2 lg:w-1/4 pb-6 ">
           <div className=" border-red-500 border md:border-2 shadow-lg rounded-2xl lg:ml-4 ">
             <img
               src={`./assets/${img}.png`}
